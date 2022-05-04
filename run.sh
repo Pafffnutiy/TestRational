57aaaa7e19756d66a82fd396b646ceb008a5fef0
Pafffnutiy <p-zil@yandex.ru>
2022-05-04 18:04:22 +0300

    Added bash script for running

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-javac -d . src/main/java/Rational.java 
+javac -d . src/main/java/Rational.java
 javac -d . -cp junit-4.13.2.jar:. src/test/java/Tests.java
 java -cp junit-4.13.2.jar:hamcrest-core-1.3.jar:. org.junit.runner.JUnitCore Tests
