f43628e40aba4db303f08d9eb4d373a5531495f3
Pafffnutiy <p-zil@yandex.ru>
2022-05-04 16:04:06 +0300

    Added new tests

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -4,6 +4,8 @@ import static java.lang.Math.abs;
 import static org.junit.Assert.*;
 
 public class Tests {
+    static final int MIN = -10000000;
+    static final int MAX = 10000000;
     @Test //№1
     public void testStandardConstructor() {
         Rational standard = new Rational();
@@ -14,8 +16,8 @@ public class Tests {
     @Test //№2
     public void testParamConstructorWithPosDenominator() {
         for (int i = 0; i < 5; ++i) {
-            int number1 = getRandomNumber(-10000000, 10000000);
-            int number2 = getRandomNumber(1, 10000000);
+            int number1 = getRandomNumber(MIN, MAX);
+            int number2 = getRandomNumber(1, MAX);
             int gcd = getGCD(number1, number2);
             Rational obj = new Rational(number1, number2);
 
@@ -27,8 +29,8 @@ public class Tests {
     @Test //№3
     public void testParamConstructorWithNegDenominator() {
         for (int i = 0; i < 5; ++i) {
-            int number1 = getRandomNumber(-1000000, 1000000);
-            int number2 = getRandomNumber(-1000000, -1);
+            int number1 = getRandomNumber(MIN, MAX);
+            int number2 = getRandomNumber(MIN, -1);
             int gcd = getGCD(number1, number2);
             Rational obj = new Rational(number1, number2);
 
@@ -40,7 +42,7 @@ public class Tests {
     @Test(expected = ArithmeticException.class) //№4
     public void testParamConstructorWithZeroDenominator() {
         int number2 = 0;
-        int number1 = getRandomNumber(-1000000, 1000000);
+        int number1 = getRandomNumber(MIN, MAX);
         int gcd = getGCD(number1, number2);
         Rational obj = new Rational(number1, number2);
     }
@@ -49,13 +51,13 @@ public class Tests {
     public void testNumeratorSetter() {
         int expectedNumerator;
         for (int i = 0; i < 5; ++i) {
-            int number1 = getRandomNumber(-1000000, 1000000);
-            int number2 = getRandomNumberNoZero(-1000000, 1000000);
+            int number1 = getRandomNumber(MIN, MAX);
+            int number2 = getRandomNumberNoZero(MIN, MAX);
             Rational obj = new Rational(number1, number2);
 
             int oldDenom = obj.getDenominator();
 
-            int newNumerator = getRandomNumber(-1000000, 1000000);
+            int newNumerator = getRandomNumber(MIN, MAX);
             obj.setNumerator(newNumerator);
             int gcd = getGCD(newNumerator, oldDenom);
 
@@ -67,14 +69,14 @@ public class Tests {
     @Test //№6
     public void testDenominatorSetter() {
         for (int i = 0; i < 5; ++i) {
-            int number1 = getRandomNumber(-1000000, 1000000);
-            int number2 = getRandomNumberNoZero(-1000000, 1000000);
+            int number1 = getRandomNumber(MIN, MAX);
+            int number2 = getRandomNumberNoZero(MIN, MAX);
             Rational obj = new Rational(number1, number2);
 
             int oldNumerator = obj.getNumerator();
 
-            int newDenominator = getRandomNumber(-1000000, 1000000);
-            if (newDenominator == 0) newDenominator += getRandomNumber(1, 1000000);
+            int newDenominator = getRandomNumber(MIN, MAX);
+            if (newDenominator == 0) newDenominator += getRandomNumber(1, MAX);
             obj.setDenominator(newDenominator);
             int gcd = getGCD(oldNumerator, newDenominator);
 
@@ -85,135 +87,166 @@ public class Tests {
         }
     }
 
-  @Test(expected = ArithmeticException.class) //№7
+    @Test(expected = ArithmeticException.class) //№7
     public void testDenominatorSetterZero() {
 
-       int number1 = getRandomNumber(-1000000, 1000000);
-       int number2 = getRandomNumberNoZero(-1000000, 1000000);
-       Rational obj = new Rational(number1, number2);
+        int number1 = getRandomNumber(MIN, MAX);
+        int number2 = getRandomNumberNoZero(MIN, MAX);
+        Rational obj = new Rational(number1, number2);
 
-       obj.setDenominator(0);
-   }
+        obj.setDenominator(0);
+    }
 
     @Test //№8
-    public void testEquals() {
-        for (int i = 0; i < 5; ++i) {
-            int number1 = getRandomNumber(-1000000, 1000000);
-            int number2 = getRandomNumberNoZero(-1000000, 1000000);
-            int number3 = getRandomNumberNoZero(-1000000, 1000000);
+    public void testToString() {
+        Rational obj1 = new Rational();
+        Rational obj2 = new Rational(5, -4);
+        Rational obj3 = new Rational(2, 3);
 
-            Rational obj1 = new Rational(number1, number2);
-            Rational obj2 = new Rational(number1, number2);
-            Rational obj3 = new Rational(getRandomNumber(-1000000, 1000000), number3);
+        String str1 = "0/1";
+        String str2 = "-5/4";
+        String str3 = "2/3";
 
-            String str1 = String.join("/", String.valueOf(number1), String.valueOf(number2));
-
-            assertTrue("Func Equals work incorrect", obj1.equals(obj2));
-            assertTrue("Func Equals work incorrect", obj2.equals(obj1));
-            assertFalse("Func Equals work incorrect", obj1.equals(obj3));
-            assertFalse("Func Equals work incorrect", obj3.equals(obj1));
-            assertFalse("Func Equals work incorrect", obj1.equals(number1));
-            assertFalse("Func Equals work incorrect", obj1.equals(str1));
-        }
+        assertEquals("Func ToString works incorrect", str1, obj1.toString());
+        assertEquals("Func ToString works incorrect", str2, obj2.toString());
+        assertEquals("Func ToString works incorrect", str3, obj3.toString());
     }
 
     @Test //№9
+    public void testEquals() {
+        for (int i = 0; i < 5; ++i) {
+            int number1 = getRandomNumber(MIN, MAX);
+            int number2 = getRandomNumberNoZero(MIN, MAX);
+            int number3 = getRandomNumberNoZero(MIN, MAX);
+
+            Rational obj1 = new Rational(number1, number2);
+            Rational obj2 = new Rational(number1, number2);
+            Rational obj3 = new Rational(getRandomNumber(MIN, MAX), number3);
+
+            String str1 = String.join("/", String.valueOf(number1), String.valueOf(number2));
+
+            assertTrue("Func Equals works incorrect", obj1.equals(obj2));
+            assertTrue("Func Equals works incorrect", obj2.equals(obj1));
+            assertFalse("Func Equals works incorrect", obj1.equals(obj3));
+            assertFalse("Func Equals works incorrect", obj3.equals(obj1));
+            assertFalse("Func Equals works incorrect", obj1.equals(number1));
+            assertFalse("Func Equals works incorrect", obj1.equals(str1));
+        }
+    }
+
+    @Test //№10
     public void testLess() {
 
         Rational obj1 = new Rational(123, 256);
         Rational obj2 = new Rational(123, 512);
 
-        assertTrue("Func less work incorrect", obj2.less(obj1));
+        assertTrue("Func less works incorrect", obj2.less(obj1));
 
         obj1 = new Rational(123, -256);
         obj2 = new Rational(123, 512);
 
-        assertTrue("Func less work incorrect", obj1.less(obj2));
+        assertTrue("Func less works incorrect", obj1.less(obj2));
 
         obj1 = new Rational(-123, -256);
         obj2 = new Rational(-321, -256);
 
-        assertTrue("Func less work incorrect", obj1.less(obj2));
+        assertTrue("Func less works incorrect", obj1.less(obj2));
 
         obj1 = new Rational(-123, 824);
         obj2 = new Rational(-321, 946);
 
-        //assertTrue("Func less work incorrect for different numerators and denominators", obj2.less(obj1));
+        assertTrue("Func less works incorrect for different numerators and denominators", obj2.less(obj1));
 
         obj1 = new Rational(-123, 256);
         obj2 = new Rational(-123, 256);
 
-        assertFalse("Func less work incorrect", obj1.less(obj2));
+        assertFalse("Func less works incorrect", obj1.less(obj2));
     }
 
-    @Test //№10
+    @Test //№11
     public void testLessOrEqual() {
 
         Rational obj1 = new Rational(123, 256);
         Rational obj2 = new Rational(123, 512);
 
-        assertTrue("Func lessOrEqual work incorrect", obj2.lessOrEqual(obj1));
+        assertTrue("Func lessOrEqual works incorrect", obj2.lessOrEqual(obj1));
 
         obj1 = new Rational(123, -256);
         obj2 = new Rational(123, 512);
 
-        assertFalse("Func lessOrEqual work incorrect because of incorrect work of Func less", obj2.lessOrEqual(obj1));
+        assertFalse("Func lessOrEqual works incorrect because of incorrect work of Func less", obj2.lessOrEqual(obj1));
 
         obj1 = new Rational(-123, 256);
         obj2 = new Rational(-123, 256);
 
-        assertTrue("Func lessOrEqual work incorrect", obj1.lessOrEqual(obj2));
-    }
-
-    @Test //№11
-    public void testPlus()
-    {
-        Rational obj1 = new Rational();
-        Rational obj2 = new Rational(654,9070);
-        Rational obj3 = new Rational(4562,34);
-        Rational obj4 = new Rational(4,3);
-        Rational obj5 = new Rational(12,-9);
-
-        assertEquals("Func Plus work incorrect", new Rational(31358062,231285), obj2.plus(obj3.plus(obj4)));
-        assertEquals("Func Plus work incorrect", obj3, obj1.plus(obj3));
-        assertEquals("Func Plus work incorrect", obj1, obj4.plus(obj5));
+        assertTrue("Func lessOrEqual works incorrect", obj1.lessOrEqual(obj2));
     }
 
     @Test //№12
-    public void testMultiply()
-    {
+    public void testPlus() {
         Rational obj1 = new Rational();
-        Rational obj2 = new Rational(654,900);
-        Rational obj3 = new Rational(456,34);
-        Rational obj4 = new Rational(3,4);
-        Rational obj5 = new Rational(12,-9);
+        Rational obj2 = new Rational(654, 9070);
+        Rational obj3 = new Rational(4562, 34);
+        Rational obj4 = new Rational(4, 3);
+        Rational obj5 = new Rational(12, -9);
 
-        assertEquals("Func Multiply work incorrect", new Rational(-16568,1275), obj2.multiply(obj3.multiply(obj5)));
-        assertEquals("Func Multiply work incorrect", new Rational(-1,1), obj4.multiply(obj5));
-        assertEquals("Func Multiply work incorrect", obj1, obj4.multiply(obj1));
+        assertEquals("Func Plus works incorrect", new Rational(31358062, 231285), obj2.plus(obj3.plus(obj4)));
+        assertEquals("Func Plus works incorrect", obj3, obj1.plus(obj3));
+        assertEquals("Func Plus works incorrect", obj1, obj4.plus(obj5));
     }
 
     @Test //№13
-    public void testMinus()
-    {
+    public void testMultiply() {
         Rational obj1 = new Rational();
-        Rational obj2 = new Rational(654,900);
-        Rational obj3 = new Rational(456,34);
-        Rational obj4 = new Rational(3,4);
-        Rational obj5 = new Rational(9,12);
+        Rational obj2 = new Rational(654, 900);
+        Rational obj3 = new Rational(456, 34);
+        Rational obj4 = new Rational(3, 4);
+        Rational obj5 = new Rational(12, -9);
 
-        assertEquals("Func Minus work incorrect", new Rational(-60869,5100), obj2.minus(obj3.minus(obj4)));
-        assertEquals("Func Minus work incorrect", obj1, obj4.minus(obj5));
-        assertEquals("Func Minus work incorrect", new Rational(-654,900), obj1.minus(obj2));
+        assertEquals("Func Multiply works incorrect", new Rational(-16568, 1275), obj2.multiply(obj3.multiply(obj5)));
+        assertEquals("Func Multiply works incorrect", new Rational(-1, 1), obj4.multiply(obj5));
+        assertEquals("Func Multiply works incorrect", obj1, obj4.multiply(obj1));
+    }
+
+    @Test //№14
+    public void testMinus() {
+        Rational obj1 = new Rational();
+        Rational obj2 = new Rational(654, 900);
+        Rational obj3 = new Rational(456, 34);
+        Rational obj4 = new Rational(3, 4);
+        Rational obj5 = new Rational(9, 12);
+
+        assertEquals("Func Minus works incorrect", new Rational(-60869, 5100), obj2.minus(obj3.minus(obj4)));
+        assertEquals("Func Minus works incorrect", obj1, obj4.minus(obj5));
+        assertEquals("Func Minus works incorrect", new Rational(-654, 900), obj1.minus(obj2));
+    }
+
+    @Test(expected = ArithmeticException.class) //№15
+    public void testDivideByZeroRational() {
+        Rational obj1 = new Rational();
+        Rational obj2 = new Rational(getRandomNumber(MIN,MAX), getRandomNumberNoZero(MIN,MAX));
+
+        obj2.divide(obj1);
+    }
+
+    @Test
+    public void testDivide() {
+        Rational obj1 = new Rational(-654, 900);
+        Rational obj2 = new Rational(-456, 34);
+        Rational obj3 = new Rational(3, 4);
+        Rational obj4 = new Rational(17, 4);
+
+        assertEquals("Func Divide works incorrect", new Rational(-76, 1), obj2.divide(obj3.divide(obj4)));
+        assertEquals("Func Divide works incorrect", new Rational(1853, 34200), obj1.divide(obj2));
     }
 
     private static int getRandomNumber(int min, int max) {
-        return (int)(Math.random()*((max-min)+1))+min;
+        return (int) (Math.random() * ((max - min) + 1)) + min;
     }
 
     private static int getRandomNumberNoZero(int min, int max) {
-        int res = (int)(Math.random()*((max-min)+1))+min;
-        if(res==0) res+=getRandomNumber(1,max);
+        int res = (int) (Math.random() * ((max - min) + 1)) + min;
+        if (res == 0) res += getRandomNumber(1, max);
 
         return res;
     }
